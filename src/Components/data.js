e39d46ea4cd39eb45ee73e7fded6ee29c46da2a8
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-07-01 16:10:45 +0530

    add data.js file

---
+++ src/Components/data.js
@@ -0,0 +1,27 @@
+dataobject = [
+    {
+        skill: "Web Design",
+        level: "advance",
+        color: "#c3DCAF"
+    },
+    {
+        skill: "Git and Github",
+        level: "intermediator",
+        color: "#E84F33"
+    },
+    {
+        skill: "ReactJs",
+        level: "beginner",
+        color: "#FF3B00"
+    },
+    {
+        skill: "Typescript",
+        level: "advance",
+        color: "#60DAFB"
+    }
+
+]
+
+export{
+    dataobject
+}
